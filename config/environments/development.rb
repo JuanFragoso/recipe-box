a2e233c3c90d965773191d2f276ade8d6c2baea5
Juan Fragoso <juan_fragoso@me.com>
2015-02-21 17:30:20 -0600

    Add users and styled

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,7 @@ RecipeBox::Application.configure do
   config.assets.debug = true
 
   Paperclip.options[:command_path] = "/usr/local/bin/"
+
+  # Needed for Devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
